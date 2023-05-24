5d71d15a67e29f57fce32b81ff50840a88833f6d
EmilioDomM <emidmor@gmail.com>
2023-05-23 18:00:45 -0600

    Palindrome (Deque)

---
+++ Leetcode_problems/9. Palindrome Number/Palindrome-Deque.cpp
@@ -0,0 +1,19 @@
+//Palindrome with deque
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        deque <int> d1;
+        deque <int> d2;
+        int num;
+        string n = to_string(x);
+        for (int i = 0; i < n.length() ; i++){
+            d1.push_back(n[i]);
+            d2.push_front(n[i]);
+        }
+        if (d1 == d2){
+            return true;
+        }else{ return false; }
+
+    }//Palindrome with deque
+};
